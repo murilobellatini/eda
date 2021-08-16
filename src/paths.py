7585ceac77144256d2713b6c89c337a0504c64be
Murilo <43999445+murilobellatini@users.noreply.github.com>
2021-08-16 15:46:12 +0200

    Add remote URLs

---
+++ src/paths.py
@@ -0,0 +1,4 @@
+# remote urls
+TEST_URL  = 'https://raw.githubusercontent.com/murilobellatini/datasets/master/titanic/test.csv'
+TRAIN_URL = 'https://raw.githubusercontent.com/murilobellatini/datasets/master/titanic/train.csv'
+DICT_URL  = 'https://raw.githubusercontent.com/murilobellatini/datasets/master/titanic/data_dictionary.txt'
